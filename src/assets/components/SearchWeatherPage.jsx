ffe3e235780fd2da059ce19a5943a1039cac30cf
MisbahAnjumShaikh <misbahanjumshaikh05pk@gmail.com>
2025-01-12 13:34:13 +0500

    intialize react bootstrap

---
+++ src/assets/components/SearchWeatherPage.jsx
@@ -0,0 +1,13 @@
+import "./SearchWeatherPage.css"
+const SearchWeatherPage = () => {
+  return (
+    <section>
+    <label htmlFor="searchInput">Enter your City Name  </label>
+    <input id="searchInput" />
+    <br/>
+    
+    </section>
+  )
+}
+
+export default SearchWeatherPage
